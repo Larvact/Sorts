5051881cc42ab1a41c76399c9a8d2c22892a10e7
Tobias Goddard <tobyg7794@hotmail.com>
2021-05-11 11:58:19 +0100

    improved the merge sort but bugs still remain. Will investigate at a later point

--- src/main/java/org/toby/model/sorts/MergeSort.java
+++ src/main/java/org/toby/model/sorts/MergeSort.java
@@ -12,59 +12,68 @@ public class MergeSort<T extends Double> extends Sort<T> {
 
     @Override
     protected void sort() {
+        mergeSort(this.unSortedList, 0 , this.unSortedList.size());
+        this.sortedList = this.unSortedList;
 
     }
 
     private void mergeSort(List<T> list, int startIndex, int endIndex){
 
 
-        if(1 < list.size()){
+        if(startIndex < endIndex){
             int middleIndex = (startIndex + endIndex) / 2;
             mergeSort(list, startIndex, middleIndex);
-            mergeSort(list, middleIndex, endIndex);
-            this.unSortedList = merge(leftSideList, rightSideList);
+            mergeSort(list, middleIndex + 1, endIndex);
+            merge(list, startIndex, middleIndex, endIndex);
         }
     }
 
-    private List<T> merge(List<T> firstOrderedList, List<T> secondOrderedList){
+    private void merge(List<T> list, int startIndex, int middleIndex, int endIndex){
 
+        List<T> firstOrderedList;
+        List<T> secondOrderedList;
 
+        firstOrderedList = new ArrayList<>();
+        for(int index = startIndex; index < middleIndex; index++){
+            firstOrderedList.add(list.get(index));
+        }
 
+        secondOrderedList = new ArrayList<>();
+        for(int index = middleIndex; index < endIndex; index++){
+            secondOrderedList.add(list.get(index));
+        }
 
         int firstListSize = firstOrderedList.size();
         int secondListSize = secondOrderedList.size();
-        List<T> mergedList = new ArrayList<>();
 
         int firstListIndex = 0;
         int secondListIndex = 0;
-        int mergedListIndex = 0;
+        int mergedListIndex = startIndex;
 
         while(firstListIndex < firstListSize && secondListIndex < secondListSize){
             if(firstOrderedList.get(firstListIndex).compareTo(secondOrderedList.get(secondListIndex)) < 0){
-                mergedList.add(mergedListIndex, firstOrderedList.get(firstListIndex));
+                list.set(mergedListIndex, firstOrderedList.get(firstListIndex));
                 mergedListIndex++;
                 firstListIndex++;
             }
             else{
-                mergedList.add(mergedListIndex, secondOrderedList.get(secondListIndex));
+                list.set(mergedListIndex, secondOrderedList.get(secondListIndex));
                 mergedListIndex++;
                 secondListIndex++;
             }
         }
 
         while(firstListIndex < firstListSize){
-            mergedList.add(mergedListIndex, firstOrderedList.get(firstListIndex));
+            list.set(mergedListIndex, firstOrderedList.get(firstListIndex));
             firstListIndex++;
             mergedListIndex++;
         }
 
         while(secondListIndex < secondListSize){
-            mergedList.add(mergedListIndex, secondOrderedList.get(secondListIndex));
+            list.set(mergedListIndex, secondOrderedList.get(secondListIndex));
             secondListIndex++;
             mergedListIndex++;
         }
-
-        return mergedList;
     }
 
 }
